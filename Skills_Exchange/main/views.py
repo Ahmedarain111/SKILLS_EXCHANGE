0d3b370d7c35eb5100e853c9d52cbb6aabb9a2ef
Ahmed <134275679+coder1467@users.noreply.github.com>
2025-10-17 19:13:34 +0500

    Add Signup page

--- Skills_Exchange/main/views.py
+++ Skills_Exchange/main/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
 
-# Create your views here.
+def signup(request):
+    if request.method == "POST":
+        username = request.POST.get("fullname")
+        username = request.POST.get("email")
+        password = request.POST.get("password")
+        confirm = request.POST.get("confirm") 
+
+        if password == confirm:
+            User.objects.create_user(username=username, password=password)
+            messages.success(request, "Account created successfully!")
+            return redirect("login")
+        else:
+            messages.error(request, "Passwords do not match!")
+
+    return render(request, "signup.html")
