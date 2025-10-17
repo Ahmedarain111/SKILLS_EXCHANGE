44996130fd3a89d6e6711ed49bdbf063c6007a8d
Ahmed <134275679+coder1467@users.noreply.github.com>
2025-10-17 19:25:33 +0500

    Add login page

--- Skills_Exchange/main/views.py
+++ Skills_Exchange/main/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
 from django.contrib import messages
 
@@ -17,3 +18,19 @@ def signup(request):
             messages.error(request, "Passwords do not match!")
 
     return render(request, "signup.html")
+
+def login(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Logged in successfully!")
+            return redirect("/")
+        else:
+            messages.error(request, "Invalid username or password")
+
+    return render(request, "login.html")
+    
